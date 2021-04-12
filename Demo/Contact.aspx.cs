0048d400620a17f4122e4b4c1d8ab50c44b45140
Akshay Jadhav <aksjadhav35@gmail.com>
2021-04-13 01:51:08 +0530

    Added code to save data in local db

--- Demo/Contact.aspx.cs
+++ Demo/Contact.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -17,6 +19,20 @@ namespace Demo
         protected void btnSend_Click(object sender, EventArgs e)
         {
             lblLabel.Text = "User " + cfMessage.Name + " Says " + cfMessage.Message;
+
+            //added code to store name and message to our local db table Message
+            var connectionString = ConfigurationManager.ConnectionStrings["messageDB"].ConnectionString;
+            var InsertStatement = "INSERT into Message (Name, Message) values (@Name, @Message)";
+            using(var sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                using (var sqlCommand = new SqlCommand(InsertStatement, sqlConnection))
+                {
+                    sqlCommand.Parameters.AddWithValue("Name", cfMessage.Name);
+                    sqlCommand.Parameters.AddWithValue("Message", cfMessage.Message);
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
